904ff156f949e1dc1017633912cc877aaa0324bf
Mikey <michaelp.esteban@gmail.com>
2021-05-03 11:08:31 -0400

    created styled components file for App

---
+++ src/components/styles/App.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  color: white;
+
+  &::after {
+    content: "";
+    background: url(${props => props.background});
+    background-attachment: fixed;
+    background-repeat: no-repeat;
+    background-position: center-top;
+    background-size: cover;
+    opacity: 0.5;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    position: absolute;
+    z-index: -1;
+  }
+
+  height: 100vh;
+  width: 100%;
+`;
+
+export { AppWrapper };
