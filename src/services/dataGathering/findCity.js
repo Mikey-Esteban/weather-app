dafed42cf3ac7054724a7c651dedcad3689525d4
Mikey <michaelp.esteban@gmail.com>
2021-05-04 13:50:11 -0400

    looking for bug

--- src/services/dataGathering/findCity.js
+++ src/services/dataGathering/findCity.js
@@ -1,4 +1,7 @@
 const findCity = (name, data) => {
+  console.log("helllooooooo");
+  console.log("name", name);
+  console.log("data", data);
   return data.filter(c => c.name.toLowerCase() === name.toLowerCase());
 };
 
