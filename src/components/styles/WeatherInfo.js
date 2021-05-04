f9f322b8f80a4194f2c0eb9af870475cfc5b6968
Mikey <michaelp.esteban@gmail.com>
2021-05-04 10:36:58 -0400

    fixed styling for WeatherInfo

--- src/components/styles/WeatherInfo.js
+++ src/components/styles/WeatherInfo.js
@@ -1,13 +1,44 @@
 import styled from "styled-components";
 
 const MainWrapper = styled.div`
+  width: 300px;
+
+  padding: 10px 20px;
+  margin-top: 20px;
+
+  background-color: rgba(0, 0, 0, 0.4);
+  color: white;
+
   span {
     font-size: 12px;
   }
 `;
 
+const HeaderWrapper = styled.div`
+  display: flex;
+
+  flex-direction: column;
+
+  img {
+    height: 96px;
+    width: 96px;
+  }
+`;
+
+const ImageWrapper = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: center;
+`;
+
 const TempWrapper = styled.div`
   display: flex;
+  justify-content: space-around;
+
+  h1 {
+    font-size: 32px;
+  }
 
   #sideTemp {
     display: flex;
@@ -18,6 +49,8 @@ const TempWrapper = styled.div`
 const TimeWrapper = styled.div`
   display: flex;
   flex-direction: column;
+
+  margin: 20px;
 `;
 
-export { MainWrapper, TempWrapper, TimeWrapper };
+export { MainWrapper, HeaderWrapper, ImageWrapper, TempWrapper, TimeWrapper };
