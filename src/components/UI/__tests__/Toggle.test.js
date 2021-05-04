5efd88e5b9d38a9513fff5a732a8b8cc3ba1b289
Mikey <michaelp.esteban@gmail.com>
2021-05-04 12:37:53 -0400

    added onClick test

--- src/components/UI/__tests__/Toggle.test.js
+++ src/components/UI/__tests__/Toggle.test.js
@@ -4,11 +4,17 @@ import Toggle from "../Toggle";
 
 describe("Toggle", () => {
   let wrapper;
+  let handleToggle = jest.fn();
   beforeEach(() => {
-    wrapper = shallow(<Toggle />);
+    wrapper = shallow(<Toggle handleToggle={handleToggle} />);
   });
 
   it("renders", () => {
     expect(wrapper).toBeTruthy();
+  });
+
+  it("clicking checkbox calls handleToggle", () => {
+    wrapper.find("#checkbox").simulate("click");
+    expect(handleToggle).toHaveBeenCalledTimes(1);
   });
 });
